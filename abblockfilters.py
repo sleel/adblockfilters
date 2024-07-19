abdcb63a3eee4bdd464205aac4585e78f0171413
sleel <30924409+sleel@users.noreply.github.com>
2024-07-19 15:30:10 +0800

    Update abblockfilters.py

--- abblockfilters.py
+++ abblockfilters.py
@@ -39,7 +39,6 @@ def CreatReadme(ruleList, fileName):
     with open(fileName, 'a') as f:
         f.write("# AdBlock DNS Filters\n")
         f.write("适用于AdGuard的去广告合并规则，每8个小时更新一次。\n")
-        f.write("个人收藏了不少广告过滤规则，但是每次往新设备添加的时候很是头疼，于是写了这个项目，定时自动获取各规则源更新，生成合并规则库。\n")
         f.write("## 说明\n")
         f.write("1. 定时从上游各规则源获取更新，合并去重。\n")
         f.write("2. 使用两组国内、两组国外 DNS 服务，分别对上游各规则源拦截的域名进行解析，去除已无法解析的域名。（上游各规则源中存在大量已无法解析的域名，无需加入拦截规则）\n")
@@ -49,18 +48,13 @@ def CreatReadme(ruleList, fileName):
         f.write("2. AdGuard 等浏览器插件使用规则1 + 规则2\n\n")
         f.write("| 规则 | 原始链接 | 加速链接 |\n")
         f.write("|:-|:-|:-|\n")
-        f.write("| 规则1：DNS 拦截 | [原始链接](https://raw.githubusercontent.com/217heidai/adblockfilters/main/rules/adblockdns.txt) | [加速链接](https://mirror.ghproxy.com/https://raw.githubusercontent.com/217heidai/adblockfilters/main/rules/adblockdns.txt) |\n")
-        f.write("| 规则2：插件拦截 | [原始链接](https://raw.githubusercontent.com/217heidai/adblockfilters/main/rules/adblockfilters.txt) | [加速链接](https://mirror.ghproxy.com/https://raw.githubusercontent.com/217heidai/adblockfilters/main/rules/adblockfilters.txt) |\n")
+        f.write("| 规则1：DNS 拦截 | [原始链接](https://raw.githubusercontent.com/sleel/adblockfilters/main/rules/adblockdns.txt) | [加速链接](https://mirror.ghproxy.com/https://raw.githubusercontent.com/sleel/adblockfilters/main/rules/adblockdns.txt) |\n")
+        f.write("| 规则2：插件拦截 | [原始链接](https://raw.githubusercontent.com/sleel/adblockfilters/main/rules/adblockfilters.txt) | [加速链接](https://mirror.ghproxy.com/https://raw.githubusercontent.com/sleel/adblockfilters/main/rules/adblockfilters.txt) |\n")
         f.write("## 上游规则源\n")
-        f.write("1. 感谢各位广告过滤规则维护大佬们的辛苦付出。\n")
-        f.write("2. 不再引用[anti-AD](https://anti-ad.net/adguard.txt)、[yhosts](https://raw.githubusercontent.com/VeleSila/yhosts/master/hosts.txt)，具体原因见[Mosney/anti-anti-AD](https://github.com/Mosney/anti-anti-AD)。\n")
-        f.write("3. 移除[Notracking blocklist](https://raw.githubusercontent.com/notracking/hosts-blocklists/master/adblock/adblock.txt)，原项目[已停止维护](https://github.com/notracking/hosts-blocklists/issues/900)。\n")
-        f.write("4. 移除[ADgk](https://raw.githubusercontent.com/banbendalao/ADgk/master/ADgk.txt)，项目超过 1 年未更新。\n")
-        f.write("\n")
         f.write("| 规则 | 类型 | 原始链接 | 加速链接 | 更新日期 |\n")
         f.write("|:-|:-|:-|:-|:-|\n")
         for rule in ruleList:
-            f.write("| %s | %s | [原始链接](%s) | [加速链接](https://mirror.ghproxy.com/https://raw.githubusercontent.com/217heidai/adblockfilters/main/rules/%s.txt) | %s |\n" % (rule[0],rule[1],rule[2],rule[0].replace(' ', '_'),rule[3]))
+            f.write("| %s | %s | [原始链接](%s) | [加速链接](https://mirror.ghproxy.com/https://raw.githubusercontent.com/sleel/adblockfilters/main/rules/%s.txt) | %s |\n" % (rule[0],rule[1],rule[2],rule[0].replace(' ', '_'),rule[3]))
 
 def GetBlackList():
     blackList = []
@@ -155,9 +149,9 @@ def CreatDNS(blockDict, unblockDict, fileName):
     with open(fileName, 'a') as f:
         f.write("!\n")
         f.write("! Title: AdBlock DNS\n")
-        f.write("! Description: 适用于AdGuard的去广告合并规则，每8个小时更新一次。规则源：1Hosts (Lite)、AdGuard Base filter、AdGuard Base filter、AdGuard DNS filter、AdRules DNS List、Hblock、NEO DEV HOST、OISD Basic、1024 hosts、ad-wars hosts、StevenBlack hosts、xinggsf、EasyList、Easylist China、EasyPrivacy、CJX's Annoyance List、SmartTV Blocklist、AWAvenue Ads Rule、jiekouAD\n")
-        f.write("! Homepage: https://github.com/217heidai/adblockfilters\n")
-        f.write("! Source: https://raw.githubusercontent.com/217heidai/adblockfilters/main/rules/adblockdns.txt\n")
+        f.write("! Description: 适用于AdGuard的去广告合并规则，每8个小时更新一次。规则源：1Hosts (Lite)、AdGuard DNS filter、AdRules DNS List、anti-AD、AWAvenue Ads Rule、Hblock、NEO DEV HOST、OISD Basic、SmartTV Blocklist\n")
+        f.write("! Homepage: https://github.com/sleel/adblockfilters\n")
+        f.write("! Source: https://raw.githubusercontent.com/sleel/adblockfilters/main/rules/adblockdns.txt\n")
         f.write("! Version: %s\n"%(time.strftime("%Y%m%d%H%M%S", time.localtime())))
         f.write("! Last modified: %s\n"%(time.strftime("%Y/%m/%d %H:%M:%S", time.localtime())))
         f.write("! Blocked domains: %s\n"%(len(blockList)))
@@ -183,9 +177,9 @@ def CreatFiter(filterList, fileName):
     with open(fileName, 'a') as f:
         f.write("!\n")
         f.write("! Title: AdBlock Filter\n")
-        f.write("! Description: 适用于AdGuard的去广告合并规则，每8个小时更新一次。规则源：1Hosts (Lite)、AdGuard Base filter、AdGuard Base filter、AdGuard DNS filter、AdRules DNS List、Hblock、NEO DEV HOST、OISD Basic、1024 hosts、ad-wars hosts、StevenBlack hosts、xinggsf、EasyList、Easylist China、EasyPrivacy、CJX's Annoyance List、SmartTV Blocklist、AWAvenue Ads Rule、jiekouAD\n")
-        f.write("! Homepage: https://github.com/217heidai/adblockfilters\n")
-        f.write("! Source: https://raw.githubusercontent.com/217heidai/adblockfilters/main/rules/adblockfilters.txt\n")
+        f.write("! Description: 适用于AdGuard的去广告合并规则，每8个小时更新一次。规则源：AdGuard Base filter、AdGuard Tracking Protection filter、AdGuard URL Tracking filter、AdGuard Chinese filter、CJX's Annoyance List、xinggsf mv、jiekouAD\n")
+        f.write("! Homepage: https://github.com/sleel/adblockfilters\n")
+        f.write("! Source: https://raw.githubusercontent.com/sleel/adblockfilters/main/rules/adblockfilters.txt\n")
         f.write("! Version: %s\n"%(time.strftime("%Y%m%d%H%M%S", time.localtime())))
         f.write("! Last modified: %s\n"%(time.strftime("%Y/%m/%d %H:%M:%S", time.localtime())))
         f.write("! Blocked Filters: %s\n"%(len(filterList)))
